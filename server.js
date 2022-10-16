971606780430e86b887abbf07b4ddef21f8ba7e5
Jérémie Nuel <jeremie.nuel@pm.me>
2022-10-16 10:23:24 +0200

    premier commit

---
+++ server.js
@@ -0,0 +1,88 @@
+const https = require('https')
+
+const express = require('express')
+const cors = require('cors')
+const mustache = require('mustache-express')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.engine('html', mustache())
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+
+const data = { pages: {} }
+
+const retourneToutesLesPages = () => {
+    
+    const data_list = JSON.stringify({
+        query: `{
+            pages {
+                list(orderBy:ID) {
+                    id
+                    title
+                    createdAt
+                    updatedAt
+                    path
+                }
+            }
+        }`
+    })
+
+    const options = {
+        hostname: 'wiki.reflux.media',
+        path: '/graphql',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data_list.length,
+            'Authorization':process.env.API_KEY,
+            'User-Agent': 'Node',
+        },
+    }
+
+    const req = https.request(options, (res) => {
+        
+        let raw_data = ''
+
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', (data) => {
+            raw_data = data
+        })
+        
+        res.on('end', () => {
+            const renderData = JSON.parse(raw_data)
+            data.pages = renderData.data.pages
+            
+        })
+
+    })
+
+    req.on('error', (error) => {
+        console.error(error)
+    })
+
+    req.write(data_list)
+    req.end()
+}
+
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 
+}
+
+app.get('/', cors(corsOptions), (req, res, next) => {
+    console.log(data.pages.list)
+    res.render('index.html', data)
+})
+
+
+app.listen(process.env.PORT || 8080, () => {
+   console.log('le serveur écoute sur le port 8080')
+   retourneToutesLesPages()
+})
