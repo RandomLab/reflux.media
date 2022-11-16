5a9dc2e851ba6878783691ac9ad93026f58d5c86
Jérémie Nuel <jeremie.nuel@pm.me>
2022-11-16 11:52:21 +0100

    function pour récupérer le contenu de la page

--- server.js
+++ server.js
@@ -16,6 +16,59 @@ app.set('views', __dirname + '/views')
 
 const data = { pages: {} }
 
+const retourneLeContenu = (id) => {
+
+    const page_content = JSON.stringify({
+        query: `{
+            pages {
+                single(id:${id}) {
+                    content
+                }
+            }
+        }`
+    })
+
+    const options = {
+        hostname: 'wiki.reflux.media',
+        path: '/graphql',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': page_content.length,
+            'Authorization':process.env.API_KEY,
+            'User-Agent': 'Node',
+        },
+    }
+
+    const req = https.request(options, (res) => {
+
+        let raw_data = ''
+
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', (data) => {
+            raw_data = data
+        })
+
+        res.on('end', () => {
+            const renderData = JSON.parse(raw_data)
+            data.pages = renderData.data.pages
+            console.log(data.pages.single.content)
+            
+        })
+
+    })
+
+    req.on('error', (error) => {
+        console.error(error)
+    })
+
+    req.write(page_content)
+    req.end()
+
+}
+
 const retourneToutesLesPages = () => {
 
     const data_list = JSON.stringify({
@@ -58,7 +111,9 @@ const retourneToutesLesPages = () => {
         res.on('end', () => {
             const renderData = JSON.parse(raw_data)
             data.pages = renderData.data.pages
-
+            data.pages.list.forEach(element => {
+                retourneLeContenu(element.id)
+            });
         })
 
     })
@@ -77,7 +132,8 @@ const corsOptions = {
 }
 
 app.get('/', cors(corsOptions), (req, res, next) => {
-    console.log(data.pages.list)
+    // console.log(data.pages.list)
+    // console.log(data)
     res.render('index.html', data)
 })
 
